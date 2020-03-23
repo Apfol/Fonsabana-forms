2514836aa21b0da723e5016979e5d6d5e48941fe
Andrés Fabián Ramos <andresfabi90@gmail.com>
2020-03-23 12:07:10 -0500

    Added Aportes Form

--- Formulario disminución o aumento de aportes/assets/js/javascript.js
+++ Formulario disminución o aumento de aportes/assets/js/javascript.js
@@ -1,8 +1,12 @@
 var signatureImg, footprintImg, identificationCardDoc;
 
 document.getElementById('print').onclick = function () {
-    var doc = demoFromHTML(signatureImg, footprintImg);
-    doc.save('table.pdf');
+    try {
+        var doc = demoFromHTML(signatureImg, footprintImg);
+        doc.save('table.pdf');
+    } catch (err) {
+        alert("Error al generar el documento, verifica que subiste toda la información requerida.");
+    }
 };
 
 const signatureFile = $("#signatureFile");
@@ -56,55 +60,44 @@ footprintFile.change(function () {
 });
 
 function demoFromHTML(signatureImg, footprintImg) {
-    var doc = new jsPDF();
+    var doc = new jsPDF({
+        format: 'a4',
+    });
 
     var logoImg = new Image();
     logoImg.src = 'assets/img/logo-fonsabana.png';
-    doc.addImage(logoImg, 'png', 16, 16, 50, 16);
-
-    var fonsabanitoImg = new Image();
-    fonsabanitoImg = 'assets/img/fonsabanito.jpg';
-    doc.addImage(fonsabanitoImg, 'jpg', 167, 15.5, 30, 18);
+    doc.addImage(logoImg, 'png', 20, 14, 50, 20);
+    var formDate = $("#formDate").val().split('-');
 
     doc.autoTable({
         theme: 'plain',
         styles: { lineColor: [142, 142, 142], lineWidth: 0.1, fontSize: 8 },
-        head: [[{ content: '', colSpan: 4, styles: { minCellHeight: 20 } }, { content: 'FONDO DE EMPLEADOS DE LA SABANA "FONSABANA" \n\n FORMULARIO DE AFILIACIÓN FONSABANITO', colSpan: 3, styles: { valign: 'middle', halign: 'center' } }, { content: 'VERSIÓN 4.\n\n ABRIL 09 DE 2018', colSpan: 2, styles: { valign: 'middle', halign: 'center' } }, { content: '', colSpan: 3, styles: { minCellHeight: 15 } }]],
-        body: [
-            [{ content: 'Fecha de solicitud: ' + $("#formDate").val(), colSpan: 3 }, { content: 'Mi nombre completo y apellidos de Fonsabanito son:\n' + $("#namesAndSurnames").val(), colSpan: 9 }],
-            [{ content: 'Tipo de documento: ' + getSelected("documentType"), colSpan: 3 }, { content: 'Mi número es: ' + $("#documentNumber").val(), colSpan: 9 }],
-            [{ content: 'Sexo: ' + getSelected("sex"), colSpan: 4 }, { content: 'Fecha de nacimiento: ' + $("#bornDate").val(), colSpan: 4 }, { content: 'Lugar de nacimiento: ' + $("#bornTown").val(), colSpan: 4 }],
-            [{ content: 'Mi dirección es: ' + $("#home").val(), colSpan: 6 }, { content: 'Barrio: ' + $("#homeTown").val(), colSpan: 6 }],
-            [{ content: 'Ciudad: ' + $("#city").val(), colSpan: 6 }, { content: 'Departamento: ' + $("#department").val(), colSpan: 6 }],
-            [{ content: 'Mi teléfono es: ' + $("#phone").val(), colSpan: 6 }, { content: 'Mi correo electrónico es: ' + $("#email").val(), colSpan: 6 }],
-            [{ content: '', styles: { fillColor: [200, 200, 200], halign: 'center' }, colSpan: 16 }],
-            [{ content: 'El nombre de mi papá es: ' + $("#fatherName").val(), colSpan: 6 }, { content: 'Su cédula es: ' + $("#fatherDocumentNumber").val(), colSpan: 6 }],
-            [{ content: 'Empresa donde trabaja mi papá: ' + $("#fatherBusiness").val(), colSpan: 6 }, { content: 'Su teléfono de oficina es: ' + $("#fatherPhone").val(), colSpan: 6 }],
-            [{ content: 'El nombre de mi mamá es: ' + $("#motherName").val(), colSpan: 6 }, { content: 'Su cédula es: ' + $("#motherDocumentNumber").val(), colSpan: 6 }],
-            [{ content: 'Empresa donde trabaja mi mamá: ' + $("#motherBusiness").val(), colSpan: 6 }, { content: 'Su teléfono de oficina es: ' + $("#motherPhone").val(), colSpan: 6 }],
-            [{ content: '', styles: { fillColor: [200, 200, 200], halign: 'center' }, colSpan: 16 }],
-            [{ content: 'Mi colegio es: ' + $("#school").val(), colSpan: 6 }, { content: 'Mi curso es: ' + $("#course").val(), colSpan: 6 }],
-            [{ content: 'Los deportes que más me gustan son: ' + $("#sport1").val() + ", " + $("#sport2").val() + ", " + $("#sport3").val() + ".", colSpan: 12 }],
-            [{ content: 'Mis pasatiempos favoritos son: ' + $("#hobby1").val() + ", " + $("#hobby2").val() + ", " + $("#hobby3").val() + ".", colSpan: 12 }],
-            [{ content: 'Asociado a Fonsabana: ' + $("#fonsabanaAssociate").val(), colSpan: 6 }, { content: 'Cédula: ' + $("#fonsabanaDocumentNumber").val(), colSpan: 6 }],
-            [{ content: 'Empresa donde trabaja: ' + $("#fonsabanaBusiness").val(), colSpan: 6 }, { content: 'Parentesco: ' + $("#fonsabanaRelationship").val(), colSpan: 6 }],
-            [{ content: '\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tFirma\t\t\t\t\t\t\t\t\t\t\t\t\tHuella', colSpan: 12 }],
-        ],
+        head: [[{ content: '', colSpan: 6, styles: { minCellHeight: 20 } }, { content: 'DISMINUCIÓN - AUMENTO DE APORTES', colSpan: 6, styles: { valign: 'middle', halign: 'center' } }, { content: 'Versión 1 \n Julio 14 de 2015', colSpan: 4, styles: { valign: 'middle', halign: 'center' } }]],
     });
+
+    doc.setFont("arial");
+    doc.setFontType("normal");
+    doc.text(20, 50, "Chía, " + formDate[2] + " de " + formDate[1] + " del " + formDate[0] + ".");
+    doc.text(20, 70, "Señores");
+    doc.setFontType("bold");
+    doc.text(20, 76, "FONDO DE EMPLEADOS DE LA SABANA");
+    doc.setFontType("normal");
+    doc.text(20, 96, "Apreciados señores:");
+    doc.text(20, 116, "Me permito autorizar a ustedes efectuar los siguientes descuentos de mi \nsueldo con destino al FONDO DE EMPLEADOS DE LA SABANA,\nFONSABANA.");
+    doc.text(20, 146, "Descontar mensualmente el " + $("#discount").val() + "% de mi sueldo básico a partir de la fecha,\ncon ajuste por exceso o defecto a la cantidad del ciento próximo como aporte\na capital y ahorro permanente de acuerdo a lo estipulado en el numeral 28 de\nlos estatutos. ");
+    if (getSelected("inputType") == "Aumentar") {
+        doc.text(20, 176, "Aumentar X Disminuir _____");
+    } else {
+        doc.text(20, 176, "Aumentar _____ Disminuir X");
+    }
+    doc.text(20, 186, "Acepto de acuerdo con la ley, los descuentos por nómina que determine la\nAsamblea de la Entidad y los ajustes que correspondan a mis obligaciones\nadquiridas con el Fondo de empleados según las normas legales vigentes. ");
+    doc.text(20, 216, "Atentamente,");
+    doc.text(20, 236, "NOMBRE: " + $("#name").val());
+    doc.text(20, 246, "FIRMA Y CÉDULA: ");
 
     var firmaImg = new Image();
     firmaImg.src = signatureImg;
-    doc.addImage(firmaImg, 'png', 36, 178, 50, 15);
-
-    var firmaImg = new Image();
-    firmaImg.src = footprintImg;
-    doc.addImage(firmaImg, 'png', 133, 163, 30, 30);
-
-    doc.setPage(2);
-    //doc.addImage(logoImg, 'png', 20, 14.2, 50, 14);
-
-    doc.setPage(3);
-    //doc.addImage(logoImg, 'png', 20, 14.2, 50, 14);
+    doc.addImage(firmaImg, 'png', 20, 253, 50, 15);
 
     function getSelected(name) {
         var radios = $('input[name=' + name + ']');
@@ -125,26 +118,30 @@ function demoFromHTML(signatureImg, footprintImg) {
 }
 
 $("#sendEmailButton").click(function () {
-    var doc = demoFromHTML(signatureImg, footprintImg);
-    $("#sendEmailButton").text("Enviando...");
-    Email.send({
-        SecureToken: "785ccc29-2210-4806-bc5e-3576e0d769e9",
-        To: ['chang.andres@hotmail.com'],
-        From: "andresfabi90@gmail.com",
-        Subject: "Nuevo Formulario Asociado - " + $("#names").val() + " " + $("#firstSurname").val(),
-        Body: $("#names").val() + " " + $("#firstSurname").val() + " te acaba de enviar su Formulario Asociado Digilenciado.",
-        Attachments: [
-            {
-                name: "Formulario.pdf",
-                data: doc.output('datauri')
-            }]
-    }).then(
-        message => {
-            $("#sendEmailButton").text("Enviar por correo electrónico");
-            alert(message)
-        }
+    try {
+        var doc = demoFromHTML(signatureImg, footprintImg);
+        $("#sendEmailButton").text("Enviando...");
+        Email.send({
+            SecureToken: "785ccc29-2210-4806-bc5e-3576e0d769e9",
+            To: ['chang.andres@hotmail.com', $("#personalEmailToSend").val(), $("#laboralEmailToSend").val()],
+            From: "andresfabi90@gmail.com",
+            Subject: "Nuevo Formulario Asociado - " + $("#names").val() + " " + $("#firstSurname").val(),
+            Body: $("#names").val() + " " + $("#firstSurname").val() + " te acaba de enviar su Formulario Asociado Digilenciado.",
+            Attachments: [
+                {
+                    name: "Formulario.pdf",
+                    data: doc.output('datauri')
+                }]
+        }).then(
+            message => {
+                $("#sendEmailButton").text("Enviar formulario");
+                alert(message)
+            }
 
-    );
+        );
+    } catch (err) {
+        alert("Error al generar el documento, verifica que subiste toda la información requerida.");
+    }
 
 });
 
