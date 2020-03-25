c72297ba4b1e82215cf9cc9ba331ea3be36ca90d
Andrés Fabián Ramos <andresfabi90@gmail.com>
2020-03-25 18:56:11 -0500

    Finished ahorro fonsabanito doc

--- Formulario ahorro fonsabanito/assets/js/javascript.js
+++ Formulario ahorro fonsabanito/assets/js/javascript.js
@@ -72,32 +72,51 @@ function demoFromHTML(signatureImg, footprintImg) {
     doc.autoTable({
         theme: 'plain',
         styles: { lineColor: [142, 142, 142], lineWidth: 0.1, fontSize: 8 },
-        head: [[{ content: '', colSpan: 6, styles: { minCellHeight: 20 } }, { content: 'DISMINUCIÓN - AUMENTO DE APORTES', colSpan: 6, styles: { valign: 'middle', halign: 'center' } }, { content: 'Versión 1 \n Julio 14 de 2015', colSpan: 4, styles: { valign: 'middle', halign: 'center' } }]],
+        head: [[{ content: '', colSpan: 6, styles: { minCellHeight: 20 } }, { content: 'SOLICITUD APERTURA AHORRO PROGRAMADO', colSpan: 6, styles: { valign: 'middle', halign: 'center' } }, { content: 'Versión 3 \n Enero 25 de 2018', colSpan: 4, styles: { valign: 'middle', halign: 'center' } }]],
     });
 
     doc.setFont("arial");
+    doc.setFontSize(11);
     doc.setFontType("normal");
-    doc.text(20, 50, "Chía, " + formDate[2] + " de " + formDate[1] + " del " + formDate[0] + ".");
-    doc.text(20, 70, "Señores");
-    doc.setFontType("bold");
-    doc.text(20, 76, "FONDO DE EMPLEADOS DE LA SABANA");
-    doc.setFontType("normal");
-    doc.text(20, 96, "Apreciados señores:");
-    doc.text(20, 116, "Me permito autorizar a ustedes efectuar los siguientes descuentos de mi \nsueldo con destino al FONDO DE EMPLEADOS DE LA SABANA,\nFONSABANA.");
-    doc.text(20, 146, "Descontar mensualmente el " + $("#discount").val() + "% de mi sueldo básico a partir de la fecha,\ncon ajuste por exceso o defecto a la cantidad del ciento próximo como aporte\na capital y ahorro permanente de acuerdo a lo estipulado en el numeral 28 de\nlos estatutos. ");
-    if (getSelected("inputType") == "Aumentar") {
-        doc.text(20, 176, "Aumentar X Disminuir _____");
-    } else {
-        doc.text(20, 176, "Aumentar _____ Disminuir X");
-    }
-    doc.text(20, 186, "Acepto de acuerdo con la ley, los descuentos por nómina que determine la\nAsamblea de la Entidad y los ajustes que correspondan a mis obligaciones\nadquiridas con el Fondo de empleados según las normas legales vigentes. ");
-    doc.text(20, 216, "Atentamente,");
-    doc.text(20, 236, "NOMBRE: " + $("#name").val());
-    doc.text(20, 246, "FIRMA Y CÉDULA: ");
+    doc.text(20, 45, "Fecha: " + formDate[2] + " de " + formDate[1] + " del " + formDate[0] + ". \t No. Solicitud: " + $("#requestNumber").val() + "\n"
+        + "Nombre Asociado: " + $("#associateName").val() + "\n"
+        + "No Identificación" + $("#identificationNumber").val() + "\n"
+        + "Entidad: " + $("#entity").val() + ".\t\t\t Dependencia: " + $("#dependence").val() + "\n"
+        + "Dirección: " + $("#direction").val() + ".\t\t\t Teléfono: " + $("#phone").val());
 
+    doc.setFontType("bold");
+    doc.text(20, 75, "CONSTITUCIÓN AHORRO");
+    doc.setFontType("normal");
+    doc.text(20, 80, "Valor cuota ahorro mensual: $" + $("#mensualSaving").val() + " a partir de: " + $("#savingDate").val() + ".\n" + "Plazo meses: " + $("#savingMonths").val() + "."
+        + "\nPago Por: Nómina: " + $("#payroll").val() + " Por Caja: " + $("#payPerBox").val()
+        + "\nMotivo constitución: " + $("#motiveConstitution").val());
+    doc.setFontType("bold");
+    doc.text(20, 102, "AUTORIZACIÓN DESCUENTO POR NÓMINA");
+    doc.setFontType("normal");
+    doc.text(20, 107, "Me permito autorizar a ustedes efectuar los siguientes descuentos de mi sueldo con destino al FONDO DE EMPLEADOS DE LA SABANA - FONSABANA:",
+        { maxWidth: 170, align: "justify" });
+    doc.text(20, 117, "1. La suma de $" + $("#discountSum").val() + " por una sola vez, correspondiente a la cuota de afiliación a la línea de ahorro programado Fonsabanito.",
+        { maxWidth: 170, align: "justify" });
+    doc.text(20, 127, "2. Descuente mensualmente de mi sueldo con destino a FonSabana, de la cual soy asociado (a) la suma mensual de $" + $("#discountMonth").val() + " por " + $("#discountMotive").val() + " meses, con destino a la constitución de ahorro programado.",
+        { maxWidth: 170, align: "justify" });
+    doc.text(20, 137, "Declaro que he leído y acepto el contrato para la constitución de ahorro programado en el Fondo de Empleados de la Sabana.",
+        { maxWidth: 170, align: "justify" });
+    doc.setFontType("bold");
+    doc.text(20, 152, "AUTORIZACIÓN TRATAMIENTO DATOS PERSONALES");
+    doc.setFontType("normal");
+    doc.text(20, 157, "Autorizo a que la información recolectada en este formulario, repose en las bases de datos del fondo y sea utilizada para los fines propios de la institución y en general, para el ejercicio del objeto social. Así, mis datos personales podrán ser procesados, recolectados, almacenados, usados y actualizados para fines administrativos, comerciales, de publicidad y contacto con FONSABANA.",
+        { maxWidth: 170, align: "justify" });
+    doc.setFontType("bold");
+    doc.text(20, 180, "DECLARACIÓN VOLUNTARIA DE ORIGEN DE FONDOS");
+    doc.setFontType("normal");
+    doc.text(20, 185, "Declaro que: el origen de los fondos que manejaré en la cuenta solicitada provienen del giro ordinario de actividades lícitas, y mi ocupación económica no es ni se relaciona con la actividad profesional de compra o venta de divisas. Igualmente declaro que el origen de mis recursos proceden del desarrollo de la(s) siguiente (s) actividad (es), ____________________________________________________ ______________________________________________________________________________________ _ ",
+        { maxWidth: 170, align: "justify" });
+    doc.text(20, 210, "Me obligo a no prestar mi cuenta y a no permitir que terceros efectúen depósitos o transferencias a mi cuenta y a no hacer pagos o transferencias desde mi cuenta a desconocidos. También declaro que la información que suministro es veraz, completa, la he proporcionado sin reticencia y si el Fondo lo solicita me obligo a entregar la información, documentos y explicaciones pertinentes. Cualquier inexactitud al respecto o al verme sindicado o involucrado por cualquier autoridad, en investigaciones relacionadas con el lavado de activos o enriquecimiento ilícito, faculta al Fondo para dar por terminado sin explicaciones el presente contrato.",
+        { maxWidth: 170, align: "justify" });
+    doc.text(20, 265, "Firma y No Identificación")
     var firmaImg = new Image();
     firmaImg.src = signatureImg;
-    doc.addImage(firmaImg, 'png', 20, 253, 50, 15);
+    doc.addImage(firmaImg, 'png', 20, 245, 50, 15);
 
     function getSelected(name) {
         var radios = $('input[name=' + name + ']');
