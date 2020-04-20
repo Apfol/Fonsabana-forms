468998c2aab0dec6623abe19864ad3fc3d068712
Andrés Fabián Ramos <andresfabi90@gmail.com>
2020-04-19 23:37:11 -0500

    CDAT Finished

--- Formulario constitución CDAT/assets/js/javascript.js
+++ Formulario constitución CDAT/assets/js/javascript.js
@@ -65,45 +65,46 @@ function demoFromHTML(signatureImg, footprintImg) {
     var logoImg = new Image();
     logoImg.src = 'assets/img/logo-fonsabana.png';
     doc.addImage(logoImg, 'png', 20, 14, 50, 20);
-    var formDate = $("#formDateSaving").val().split('-');
 
     doc.autoTable({
         theme: 'plain',
         styles: { lineColor: [142, 142, 142], lineWidth: 0.1, fontSize: 8 },
-        head: [[{ content: '', colSpan: 6, styles: { minCellHeight: 20 } }, { content: 'SOLICITUD AUTORIZACIÓN CONSTITUCIÓN CUPO CRECE', colSpan: 6, styles: { valign: 'middle', halign: 'center' } }, { content: 'Versión 2 \n Septiembre 12 de 2019', colSpan: 4, styles: { valign: 'middle', halign: 'center' } }]],
+        head: [[{ content: '', colSpan: 6, styles: { minCellHeight: 20 } }, { content: 'CONSTITUCIÓN CDAT', colSpan: 6, styles: { valign: 'middle', halign: 'center' } }, { content: 'Versión 2 \n Octubre 09 de 2019', colSpan: 4, styles: { valign: 'middle', halign: 'center' } }]],
     });
 
     doc.setFont("arial");
-    doc.setFontSize(11);
-    doc.setFontType("normal");
-    doc.text(20, 45, "Fecha: " + formDate[2] + " de " + formDate[1] + " del " + formDate[0] + ". \t No. Solicitud: " + $("#requestNumber").val() + "\n"
-        + "Nombre Asociado: " + $("#associateName").val() + "\n"
-        + "No Identificación" + $("#identificationNumber").val() + "\n"
-        + "Entidad: " + $("#entity").val() + ".\t\t\t Dependencia: " + $("#dependence").val() + "\n"
-        + "Dirección: " + $("#direction").val() + ".\t\t\t Teléfono: " + $("#phone").val());
+    doc.setFontSize(14);
 
     doc.setFontType("bold");
-    doc.text(20, 75, "CONSTITUCIÓN AHORRO");
+    doc.text(20, 55, "DATOS GENERALES");
     doc.setFontType("normal");
-    doc.text(20, 80, "Valor cuota ahorro mensual: $" + $("#mensualSaving").val() + " a partir de: " + $("#savingDate").val() + ".\n" + "Plazo meses: " + $("#savingMonths").val() + "."
-        + "\nPago Por: Nómina: " + $("#payroll").val() + " Por Caja: " + $("#payPerBox").val()
-        + "\nMotivo constitución: " + $("#motiveConstitution").val());
+    doc.text(20, 62, "Nombre Asociado: " + $("#associateName").val()
+        + "\nNo de Identificación: " + $("#identificationNumber").val() + "\tEmpresa: " + $("#business").val()
+        + "\nDirección: " + $("#direction").val() + "\tTeléfono: " + $("#phone").val()
+        + "\nCiudad: " + $("#city").val());
     doc.setFontType("bold");
-    doc.text(20, 102, "AUTORIZACIÓN DESCUENTO POR NÓMINA");
+    doc.text(20, 90, "CONSTITUCIÓN CDAT");
     doc.setFontType("normal");
-    doc.text(20, 107, "Autorizo al señor pagador de " + $("#payerQuantity").val() + " para que descuente mensualmente de mi sueldo con destino al Fondo de Empleados de la Sabana - Fonsabana, de la cual soy asociado (a) la suma mensual de $" + $("#sumMonth").val() + " por " + $("#monthQuantity").val() + " meses, con destino a la constitución de ahorro programado. Declaro que he leído y acepto el contrato para la constitución de ahorro programado en el Fondo de Empleados de la Sabana",
-        { maxWidth: 170, align: "justify" });
+    doc.text(20, 97, "VALOR: " + $("#value").val()
+        + "\nPRIMER TITULAR (asociado)"
+        + "\nSEGUNDO TITULAR (opcional)"
+        + "\nNombres y Apellidos: " + $("#titularNames").val()
+        + "\nNo de identificación: " + $("#titularIdentificationNumber").val() + "\tde " + $("#titularidentificationPlace").val()
+        + "\nFECHA DE CONSTITUCIÓN: " + $("#titularConstitutionDate").val()
+        + "\nPlazo: " + getSelected("term")
+        + "\nTASA DE INTERÉS PACTADA: " + $("#interestRate").val() + "% EA " + $("#ea").val() + "% Nominal");
+    doc.text(20, 148, "Declaro que he leído y acepto el reglamento para la constitución del CDAT en el Fondo de Empleados de La Sabana.", { maxWidth: 170, align: "justify" });
     doc.setFontType("bold");
-    doc.text(20, 130, "DECLARACIÓN VOLUNTARIA DE ORIGEN DE FONDOS");
+    doc.text(20, 162, "DECLARACIÓN VOLUNTARIA DE ORIGEN DE FONDOS");
     doc.setFontType("normal");
-    doc.text(20, 135, "Declaro que: el origen de los fondos que manejaré en la cuenta solicitada provienen del giro ordinario de actividades lícitas, y mi ocupación económica no es ni se relaciona con la actividad profesional de compra o venta de divisas. Igualmente declaro que el origen de mis recursos proceden del desarrollo de la(s) siguiente (s) actividad (es), " + $("#activities").val() + ".",
+    doc.text(20, 169, "Declaro que: el origen de los fondos que manejaré en la cuenta solicitada provienen del giro ordinario de actividades lícitas, y mi ocupación económica no es ni se relaciona con la actividad profesional de compra o venta de divisas. Igualmente declaro que el origen de mis recursos proceden del desarrollo de la(s) siguiente (s) actividad (es), " + $("#activities").val() + ".",
         { maxWidth: 170, align: "justify" });
-    doc.text(20, 155, "Me obligo a no prestar mi cuenta y a no permitir que terceros efectúen depósitos o transferencias a mi cuenta y a no hacer pagos o transferencias desde mi cuenta a desconocidos. También declaro que la información que suministro es veraz, completa, la he proporcionado sin reticencia y si el Fondo lo solicita me obligo a entregar la información, documentos y explicaciones pertinentes. Cualquier inexactitud al respecto o al verme sindicado o involucrado por cualquier autoridad, en investigaciones relacionadas con el lavado de activos o enriquecimiento ilícito, faculta al Fondo para dar por terminado sin explicaciones el presente contrato.",
+    doc.text(20, 200, "Me obligo a no prestar mi cuenta y a no permitir que terceros efectúen depósitos o transferencias a mi cuenta y a no hacer pagos o transferencias desde mi cuenta a desconocidos. También declaro que la información que suministro es veraz, completa, la he proporcionado sin reticencia y si el Fondo lo solicita me obligo a entregar la información, documentos y explicaciones pertinentes. Cualquier inexactitud al respecto o al verme sindicado o involucrado por cualquier autoridad, en investigaciones relacionadas con el lavado de activos o enriquecimiento ilícito, faculta al Fondo para dar por terminado sin explicaciones el presente contrato.",
         { maxWidth: 170, align: "justify" });
-    doc.text(20, 265, "Firma y No Identificación")
+    doc.text(20, 280, "Firma y No Identificación")
     var firmaImg = new Image();
     firmaImg.src = signatureImg;
-    doc.addImage(firmaImg, 'png', 20, 245, 50, 15);
+    doc.addImage(firmaImg, 'png', 20, 255, 50, 15);
 
     function getSelected(name) {
         var radios = $('input[name=' + name + ']');
